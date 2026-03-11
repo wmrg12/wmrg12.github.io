728a9b8c3b3a60eb9b24afb47e7db7a0bb73ae96
Vicente Guzman Aliza Abigail <alizaabigailvicenteguzman@gmail.com>
2026-03-11 09:50:17 -0400

    Primer commit portafolio

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById("cv").onclick = function(){
+    alert("Descargando CV...");
+    }
+    
+    document.getElementById("contact").onclick = function(){
+    alert("Abriendo contacto...");
+    }
